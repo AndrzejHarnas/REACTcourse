f8b1b2c34cb1013d36f3c86a51d160e47d739756
Harnas_20 <Harnas_20@o2.pl>
2019-11-26 12:48:12 +0100

    props, state, hooks

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -1,20 +1,44 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  render() {
-    return (
+const app = props => {
+const [personsState, setPersonsState] = useState({
+  persons: [
+    {name: 'Max', age: 28 },
+    {name: 'Manu', age: 29 },
+    {name: 'Stephany', age: 26 }
+  ]
+});
+
+const [otherState, serOtherState] = useState('some other value');
+
+
+const switchNameHandler = () => {
+//    console.log('i was cklicked');
+ setPersonsState({
+   persons: [
+       {name: 'Maximilian', age: 28 },
+       {name: 'Manu', age: 29 },
+       {name: 'Stephany', age: 27 }
+     ]
+   });
+};
+
+
+
+return (
       <div className="App">
         <h1>Hi i'am React APP </h1>
         <p> This is really working </p>
-        <Person />
+        <button onClick={switchNameHandler}> Switch Name </button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: racing </Person>
+        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
       </div>
     );
 
-//    return React.createElement('div', {className: 'App'} , React.createElement('h1', null, 'Hi its working!!!'))
-  }
 }
 
-export default App;
+export default app;
--- react-guide/src/Person/Person.js
+++ react-guide/src/Person/Person.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const Person = () => {
+const Person = (props) => {
 
 return(
-  <p> I'm a Person! </p>
+  <div>
+  <p> I'm a {props.name} and i am {props.age}  years old! </p>
+  <p>{props.children}</p>
+  </div>
 )
 
 }
