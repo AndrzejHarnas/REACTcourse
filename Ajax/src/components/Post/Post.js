4c3dad0757bac68fe7fce84bdd39ed2f503fe57e
Harnas_20 <Harnas_20@o2.pl>
2020-04-21 14:30:09 +0200

    styling active route

--- Ajax/src/components/Post/Post.js
+++ Ajax/src/components/Post/Post.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
+
 
 import './Post.css';
 
 const post = (props) => {
-  console.log(props);
   return(
     <article className="Post" onClick={props.clicked}>
         <h1>{props.title}</h1>
@@ -15,4 +14,4 @@ const post = (props) => {
   );
 };
 
-export default withRouter(post);
+export default post;
