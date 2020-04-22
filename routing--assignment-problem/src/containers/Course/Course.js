cda4440be27cd2ea010cb1e7c22089113f8c124a
Harnas_20 <Harnas_20@o2.pl>
2020-04-22 15:14:18 +0200

    routing exercise part 1

---
+++ routing--assignment-problem/src/containers/Course/Course.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+
+class Course extends Component {
+    render () {
+        return (
+            <div>
+                <h1>_COURSE_TITLE_</h1>
+                <p>You selected the Course with ID: _ID_</p>
+            </div>
+        );
+    }
+}
+
+export default Course;
