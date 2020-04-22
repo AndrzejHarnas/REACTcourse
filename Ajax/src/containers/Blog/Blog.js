eafd25538f5e34ea2e9bda6153ac52d4a1edc170
Harnas_20 <Harnas_20@o2.pl>
2020-04-22 09:07:18 +0200

    route Switch component

--- Ajax/src/containers/Blog/Blog.js
+++ Ajax/src/containers/Blog/Blog.js
@@ -5,7 +5,7 @@ import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 import FullPost from './FullPost/FullPost';
 
-import {Route, NavLink} from 'react-router-dom';
+import {Route, NavLink, Switch} from 'react-router-dom';
 
 import './Blog.css';
 
@@ -37,10 +37,11 @@ class Blog extends Component {
               </header>
               {/*<Route path="/" exact render={ () => <h1> Home </h1> } />
               <Route path="/" render={ () => <h1> Home 2 </h1> } />*/}
-              <Route path="/" exact component={Posts} />
-              <Route path="/new-post" exact component={NewPost} />
-              <Route path="/:id" exact component={FullPost} />
-
+              <Switch>
+                <Route path="/" exact component={Posts} />
+                <Route path="/new-post" exact component={NewPost} />
+                <Route path="/:id" exact component={FullPost} />
+              </Switch>
             </div>
         );
     }
