8109ed9afa581f928b2aaa7b19494c21f028f50f
Harnas_20 <Harnas_20@o2.pl>
2020-04-22 10:21:18 +0200

    Route Redirect component

--- Ajax/src/containers/Blog/Blog.js
+++ Ajax/src/containers/Blog/Blog.js
@@ -5,7 +5,7 @@ import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 
 
-import {Route, NavLink, Switch} from 'react-router-dom';
+import {Route, NavLink, Switch, Redirect} from 'react-router-dom';
 
 import './Blog.css';
 
@@ -41,6 +41,8 @@ class Blog extends Component {
               <Switch>
                 <Route path="/new-post" exact component={NewPost} />
                 <Route path="/posts" component={Posts} />
+                <Redirect from="/" to="/posts" />
+                //<Route path="/" component={Posts} />
               </Switch>
             </div>
         );
