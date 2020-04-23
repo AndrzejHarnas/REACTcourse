ca309a96b0dafdc05c3f73e3238839b380dc9dbd
Harnas_20 <Harnas_20@o2.pl>
2020-04-23 14:46:21 +0200

    Cotact Data Component

--- burger/src/containers/Checkout/Checkout.js
+++ burger/src/containers/Checkout/Checkout.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import {Route} from 'react-router-dom';
+import ContactData from "./ContactData/ContactData";
 
 
 class Checkout extends Component {
@@ -38,6 +40,7 @@ render () {
         checkooutCanceled = { this.checkoutCanceledHandler }
         checkoutContinued = { this.checkContinuedHandler  }
         />
+        <Route path={this.props.match.path + '/contact-data'} component={ContactData} />
       </div>
     )
   }
