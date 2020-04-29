dd4c1b212c30900ab0867ed9fe4886973464651f
Harnas_20 <Harnas_20@o2.pl>
2020-04-29 10:38:30 +0200

    reduz action exercise

--- redux--01-start/src/store/reducer.js
+++ redux--01-start/src/store/reducer.js
@@ -3,10 +3,43 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
-  if(action.type === 'INCREMENT') {
-    return {
-      counter: state.counter + 1
-    }
+
+    //     if(action.type === 'INCREMENT') {
+    // return {
+    //   counter: state.counter + 1
+    // }
+
+
+switch(action.type){
+  case 'INCREMENT':
+  return {
+    counter: state.counter + 1
+  };
+
+  case 'DECREMENT':
+  return {
+    counter: state.counter - 1
+  }
+
+  case 'ADD':
+  return {
+    counter: state.counter + 5
+  }
+
+  case 'SUB':
+  return {
+    counter: state.counter - 5
+  }
+
+
+  default:
+  console.log('nothing');
+
+
+
+
+
+
   }
   return state;
 };
