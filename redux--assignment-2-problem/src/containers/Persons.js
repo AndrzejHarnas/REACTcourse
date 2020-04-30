2733ac9bed98640403855ded060a261804645092
Harnas_20 <Harnas_20@o2.pl>
2020-04-30 09:40:17 +0200

    local state with redux

--- redux--assignment-2-problem/src/containers/Persons.js
+++ redux--assignment-2-problem/src/containers/Persons.js
@@ -6,6 +6,26 @@ import AddPerson from '../components/AddPerson/AddPerson';
 import {connect} from 'react-redux';
 
 class Persons extends Component {
+    state = {
+        persons: []
+    }
+
+    personAddedHandler = () => {
+        const newPerson = {
+            id: Math.random(), // not really unique but good enough here!
+            name: 'Max',
+            age: Math.floor( Math.random() * 40 )
+        }
+        this.setState( ( prevState ) => {
+            return { persons: prevState.persons.concat(newPerson)}
+        } );
+    }
+
+    personDeletedHandler = (personId) => {
+        this.setState( ( prevState ) => {
+            return { persons: prevState.persons.filter(person => person.id !== personId)}
+        } );
+    }
 
     render () {
         return (
@@ -31,7 +51,7 @@ const mapStageToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    onAddPerson: () => dispatch({type: actionTypes.ADD_PERSON}),
+    onAddPerson: (name, age) => dispatch({type: actionTypes.ADD_PERSON, personData: {name: name, age: age}}),
     onDeletePerson: (id) => dispatch({type: actionTypes.DELETE_PERSON, personId: id})
   }
 }
