de5258035755626acf0cc62ea3336abbf4c774fc
Harnas_20 <Harnas_20@o2.pl>
2020-05-07 13:42:14 +0200

    redux-thunk

---
+++ redux--01-start/src/store/actions/result.js
@@ -0,0 +1,26 @@
+import * as actionTypes from './actionTypes';
+
+
+export const deleteResult = (resElId) => {
+  return {
+    type: actionTypes.DELETE_RESULT,
+    id: resElId
+  }
+};
+
+export const saveResult = (res) => {
+  return {
+    type: actionTypes.STORE_RESULT,
+    result: res
+  }
+}
+
+export const storedResult = (res) => {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(saveResult(res));
+    },2000);
+  }
+
+
+};
